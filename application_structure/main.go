bad39ec4de025beb36218e6cea98e86d565c7e06
Karan <karanvknarayanan@gmail.com>
2020-11-09 21:52:40 +0530

    first commit

---
+++ application_structure/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+const secondsInHour = 3600
+
+func main() {
+	fmt.Println(secondsInHour)
+	distance:= 60.8
+	fmt.Printf("The distance in miles %f \n",distance*0.62137)
+}
